24ad68cf00dfb2ade8bc88734fde0404f827d529
dongruixiao <dongruixiao@megvii.com>
2021-03-09 23:36:26 +0800

    update leetcode 001

---
+++ 001-050/001.py
@@ -0,0 +1,24 @@
+# https://leetcode-cn.com/problems/two-sum/
+
+
+def two_sum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    assert len(nums) >= 2
+
+    hash_map = {}
+
+    for idx, num in enumerate(nums):
+        diff = target - num
+
+        if diff in hash_map:
+            return idx, hash_map[diff]
+            
+        hash_map[num] = idx
+
+
+__name__ == '__main__' and print(two_sum([1, 2, 3, 4, 5, 6, 7], 11))
